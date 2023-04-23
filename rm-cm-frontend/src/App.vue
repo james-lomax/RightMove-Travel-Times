a38a2221af4c1f79064b91501a83da5cbfb654e6
James Lomax <james.lmx@gmail.com>
2023-04-23 14:22:58 +0100

    Basic GPT generated listing web page

--- rm-cm-frontend/src/App.vue
+++ rm-cm-frontend/src/App.vue
@@ -1,47 +1,75 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <h1>Apartment Listings</h1>
+    <ApartmentItem
+      v-for="apartmentData in apartments"
+      :key="apartmentData.apartment.listing_url"
+      :apartment="apartmentData.apartment"
+      :journeys="apartmentData.journeys"
+    />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import ApartmentItem from './components/ApartmentItem.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  name: 'App',
+  components: {
+    ApartmentItem,
+  },
+  data() {
+    return {
+      apartments: [
+        {
+            "apartment": {
+                "listing_url": "https://www.rightmove.co.uk/properties/133891604#/?channel=RES_LET",
+                "thumbnail_url": "https://media.rightmove.co.uk:443/dir/crop/10:9-16:9/118k/117484/133891604/117484_LND210457_L_IMG_00_0000_max_476x317.jpeg",
+                "price_pcm": 815,
+                "address": "Northesk House, Tent Street, E1",
+                "description": "Double Room in Flat Share- Whitechapel Rent is \u00a3815 per month INCLUDING ALL BILLS **ZERO DEPOSIT OPTION AVAILABLE** Brand New Flooring & Furniture Provided!!! Sharing with 2 Female Tenants The Flat comprises of four Large Double Roo..."
+            },
+            "journeys": {
+                "Work": {
+                    "travel_times": {
+                        "walk_travel_time_minutes": 61,
+                        "transit_time_minutes": 27,
+                        "bike_time_minutes": 22
+                    }
+                }
+            }
+        },
+        {
+            "apartment": {
+                "listing_url": "https://www.rightmove.co.uk/properties/133916501#/?channel=RES_LET",
+                "thumbnail_url": "https://media.rightmove.co.uk:443/dir/crop/10:9-16:9/157k/156386/133916501/156386_11955373_IMG_00_0000_max_476x317.jpeg",
+                "price_pcm": 1240,
+                "address": "Hosta House, Beaumont Road, E10",
+                "description": "** L&Q ONLY ACCEPTS EMAIL ENQUIRIES FOR THIS APARTMENT, PLEASE KINDLY DO NOT CALL. ** This property is offered on the Intermediate Market Rent scheme and eligibility criteria will apply. L&Q are pleased to present this two bedroom, second floor apartment in Leyton. This apartme..."
+            },
+            "journeys": {
+                "Work": {
+                    "travel_times": {
+                        "walk_travel_time_minutes": 152,
+                        "transit_time_minutes": 49,
+                        "bike_time_minutes": 52
+                    }
+                }
+            }
+        },
+      ],
+    };
+  },
+};
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
---
+++ rm-cm-frontend/src/components/ApartmentItem.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="apartment-item">
+      <a :href="apartment.listing_url" target="_blank">
+        <img :src="apartment.thumbnail_url" alt="Apartment Thumbnail" />
+        <h3>{{ apartment.address }}</h3>
+      </a>
+      <p>Price per month: £{{ apartment.price_pcm }}</p>
+      <p>Description: {{ apartment.description }}</p>
+      <div class="journeys">
+        <h4>Journeys to Work:</h4>
+        <p>Walking: {{ journeys.Work.travel_times.walk_travel_time_minutes }} minutes</p>
+        <p>Transit: {{ journeys.Work.travel_times.transit_time_minutes }} minutes</p>
+        <p>Biking: {{ journeys.Work.travel_times.bike_time_minutes }} minutes</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      apartment: Object,
+      journeys: Object,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .apartment-item {
+    border: 1px solid #ccc;
+    padding: 16px;
+    margin-bottom: 16px;
+  }
+  
+  .apartment-item img {
+    width: 100%;
+    max-height: 200px;
+    object-fit: cover;
+  }
+  
+  .apartment-item h3 {
+    margin: 8px 0;
+    color: #333;
+  }
+  
+  .apartment-item a {
+    text-decoration: none;
+  }
+  
+  .journeys {
+    margin-top: 8px;
+  }
+  </style>
+  
