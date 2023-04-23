17f19301a0c1d8ad0ae226feebb6d6069da20316
James Lomax <james.lmx@gmail.com>
2023-04-23 15:47:43 +0100

    Make thumbnail clickable

--- rm-cm-frontend/src/components/ApartmentItem.vue
+++ rm-cm-frontend/src/components/ApartmentItem.vue
@@ -3,7 +3,9 @@
     <v-card-content>
     <v-row>
         <v-col cols="4">
-        <v-img :src="apartment.thumbnail_url" :alt="apartment.address" aspect-ratio="1.5"></v-img>
+            <a :href="apartment.listing_url" target="_blank">
+                <v-img :src="apartment.thumbnail_url" :alt="apartment.address" aspect-ratio="1.5"></v-img>
+            </a>
         </v-col>
         <v-col cols="8">
         <v-card-title>
