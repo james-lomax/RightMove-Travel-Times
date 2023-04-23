3387536db95c120bd7587946f0d784063959a4d1
James Lomax <james.lmx@gmail.com>
2023-04-23 16:20:34 +0100

    Load listings dynamically

--- README.md
+++ README.md
@@ -8,7 +8,21 @@ Scraping is done by scrapestorm, outputting a CSV file.
 
 Python code to processing the CSV data resulting from scrapestorms run into relevant details, compute journey times to configured destinations and output the annotated data.
 
+Run `python journeys.py` to turn apartments.csv into output.json. Note the local caching used here means subsequent runs require fewer requests.
+
 ## frontend/
 
-TODO!
-Vue.js frontend to view the sorted lists of apartments.
+Vue.js/veutify frontend to view the sorted lists of apartments.
+
+Start web server to provide outputs.json:
+
+```
+cd journeys
+python cors_server.py 3095
+```
+
+Then run with:
+
+```
+yarn dev
+```
--- rm-cm-frontend/src/App.vue
+++ rm-cm-frontend/src/App.vue
@@ -22,42 +22,27 @@ export default {
       apartments: [
       {
             "apartment": {
-                "listing_url": "https://www.rightmove.co.uk/properties/133891604#/?channel=RES_LET",
-                "thumbnail_url": "https://media.rightmove.co.uk:443/dir/crop/10:9-16:9/118k/117484/133891604/117484_LND210457_L_IMG_00_0000_max_476x317.jpeg",
-                "price_pcm": 815,
-                "address": "Northesk House, Tent Street, E1",
-                "description": "Double Room in Flat Share- Whitechapel Rent is \u00a3815 per month INCLUDING ALL BILLS **ZERO DEPOSIT OPTION AVAILABLE** Brand New Flooring & Furniture Provided!!! Sharing with 2 Female Tenants The Flat comprises of four Large Double Roo..."
+                "listing_url": "",
+                "thumbnail_url": "",
+                "price_pcm": 999,
+                "address": "Loading listings..",
+                "description": ""
             },
             "journeys": {
-                "Work": {
-                    "travel_times": {
-                        "walk_travel_time_minutes": 61,
-                        "transit_time_minutes": 27,
-                        "bike_time_minutes": 22
-                    }
-                }
-            }
-        },
-        {
-            "apartment": {
-                "listing_url": "https://www.rightmove.co.uk/properties/133916501#/?channel=RES_LET",
-                "thumbnail_url": "https://media.rightmove.co.uk:443/dir/crop/10:9-16:9/157k/156386/133916501/156386_11955373_IMG_00_0000_max_476x317.jpeg",
-                "price_pcm": 1240,
-                "address": "Hosta House, Beaumont Road, E10",
-                "description": "** L&Q ONLY ACCEPTS EMAIL ENQUIRIES FOR THIS APARTMENT, PLEASE KINDLY DO NOT CALL. ** This property is offered on the Intermediate Market Rent scheme and eligibility criteria will apply. L&Q are pleased to present this two bedroom, second floor apartment in Leyton. This apartme..."
-            },
-            "journeys": {
-                "Work": {
-                    "travel_times": {
-                        "walk_travel_time_minutes": 152,
-                        "transit_time_minutes": 49,
-                        "bike_time_minutes": 52
-                    }
-                }
             }
         },
       ],
     };
   },
+  mounted() {
+    this.updateData()
+  },
+  methods: {
+    async updateData() {
+      let rsp = await fetch('http://localhost:3095/output.json')
+      let data = await rsp.json()
+      this.apartments = data.apartments
+    }
+  }
 };
 </script>
